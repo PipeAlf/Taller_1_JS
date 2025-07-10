ac92f6d2fed3d816f29e322ade4bc2adc5a824b8
PipeAlf <pipealf1996@gmail.com>
2025-07-10 16:03:44 -0500

    Ingreso por teclado 1

---
+++ Ingreso_Teclado_1.js
@@ -0,0 +1,13 @@
+// ! Andrés Alfonso
+
+const prompt = require ("prompt-sync")();
+
+let año_nacimiento;
+año_nacimiento = prompt("Ingrese su año de nacimiento: ");
+
+let año_actual = 2025;
+let edad;
+
+edad = año_actual - año_nacimiento;
+
+console.log("Su edad es: ", edad)
